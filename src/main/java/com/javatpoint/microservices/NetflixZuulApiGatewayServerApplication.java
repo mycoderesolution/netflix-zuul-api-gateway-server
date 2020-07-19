aa6bb252c5540d8b36a99737ae002015184ef614
praveshasiwal <impraveshasiwal@gmail.com>
2020-07-19 21:42:18 +0530

    Add sleuth-zipkin to trace logs using RabbitMQ

--- src/main/java/com/javatpoint/microservices/NetflixZuulApiGatewayServerApplication.java
+++ src/main/java/com/javatpoint/microservices/NetflixZuulApiGatewayServerApplication.java
@@ -3,16 +3,18 @@ package com.javatpoint.microservices;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import brave.sampler.Sampler;
 
-@EnableZuulProxy
-@EnableDiscoveryClient
 @Configuration
+@EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class NetflixZuulApiGatewayServerApplication {
 
 	public static void main(String[] args) {
